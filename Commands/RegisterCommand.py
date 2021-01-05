a0f6b48e021674fcb1f32d816ad7296179729dfa
Hussein AlHroub <dlight161@gmail.com>
2021-01-05 20:21:11 +0000

    Fixed some shit

--- Commands/RegisterCommand.py
+++ Commands/RegisterCommand.py
@@ -10,7 +10,7 @@ class RegisterCommand:
     if len(self.commandArgs) == 0:
       return f"Error: summoner name wasn't provided, here is an example on how to use this command\n!lregister \"3amo Draven\"" 
     summonerName = self.commandArgs[0]
-    if dbutils.isSummonerExist(summonerName):
+    if dbutils.isSummonerExist(summonerName.lower()):
       return f"Already have registerred summoner with name {summonerName}"
     accountId = lolApiUtils.getAccountIdByName(summonerName)
     if(dbutils.isAccountIdExist(accountId)):
