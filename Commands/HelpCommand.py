086ffe011872ec6d594c4f18bfede74283d7ba67
Hussein <husseinhroub311@gmail.com>
2021-02-05 16:45:53 +0200

    Added help commandu

--- Commands/HelpCommand.py
+++ Commands/HelpCommand.py
@@ -1,6 +1,6 @@
 from RankExtractors.SimpleRankExtractor import SimpleRankExtractor
 
 
-class RankCommand:
+class HelpCommand:
     def execute(self):
         return 'Current avaiable commands to use:\n!lregister "Summoner Name"\n!lrank "Summoner Name"\n!ltop\n!ltop kda\n!ltop kills \n!ltop deaths\n!ltop assists \n!ltop farms\n!ltop games\n!lworst\n!lworst kda\n!lworst kills \n!lworst deaths\n!lworst assists \n!lworst farms\n!lworst games'
