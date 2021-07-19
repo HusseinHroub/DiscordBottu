27e4e7d19dc6b5ab3e0c7ba0eab5954ea08f5497
Hussein <husseinhroub311@gmail.com>
2021-07-20 00:01:04 +0300

    Adding some things

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip3 install -Iv discord==1.7.3
+pip3 install -Iv flask==1.1.2
+pip3 install -Iv ratelimit==2.2.1
+pip3 install mysql-connector-python==8.0.23
+pip3 install PyMySQL==1.0.2
