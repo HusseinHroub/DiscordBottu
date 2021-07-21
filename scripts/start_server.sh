4805fe684a06aa2463cf3242093c13b8bed1a795
Hussein <husseinhroub311@gmail.com>
2021-07-21 12:30:08 +0300

    made logs directory

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /mnt/RankBotResoources/init_env_variables.sh
+mkdir -p /mnt/RankBotResoources/DiscordBotApplicationi/logs
 nohup python3 -u /mnt/RankBotResoources/DiscordBotApplicationi/main.py > /mnt/RankBotResoources/DiscordBotApplicationi/logs/log.out 2> /mnt/RankBotResoources/DiscordBotApplicationi/logs/log.err &
 echo 'server started'
